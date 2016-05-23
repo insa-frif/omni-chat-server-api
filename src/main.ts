3885773f41355aa2a2e2d57b461985f512534582
Charles "Demurgos" Samborski <demurgos@demurgos.net>
2016-05-23 02:19:49 +0200

    Implement driver-proxy for facebook and skype

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import * as http from "http";
 import {app} from "./app";
+import {startProxy} from "./sockets/driver-proxy";
 
 export {app} from "./app";
 export {router} from "./router";
+export {startProxy} from "./sockets/driver-proxy";
 
 // Run the server if this is the root module
 if (require.main === module) {
@@ -10,6 +12,8 @@ if (require.main === module) {
   const PORT = 8080;
 
   let server = http.createServer(app);
+
+  startProxy(server);
 
   server.listen(PORT, HOST, () => {
     console.log(`Server running at http://${HOST}:${PORT}/`);
---
+++ src/sockets/driver-proxy.ts
@@ -0,0 +1,137 @@
+import * as socketIO from "socket.io";
+import * as palantiri from "palantiri-interfaces";
+import * as skypeDriver from "palantiri-driver-skype";
+import * as facebookDriver from "palantiri-driver-facebook";
+import * as Bluebird from "bluebird";
+import {Server as HttpServer} from "http";
+
+export function startProxy (server: HttpServer) {
+  let ioServer = socketIO(server);
+
+  ioServer.on("connection", (socket: any) => {
+    console.log(`Connection ${socket.id}`);
+    bindEvents(socket);
+  });
+}
+
+interface SocketContext {
+  id: string;
+  driverName: string;
+  connection: palantiri.Connection;
+  api: palantiri.Api;
+}
+
+let contexts: {[id: string]: SocketContext} = {};
+function getContext(id: string) {
+  if (!(id in contexts)) {
+    contexts[id] = {
+      id: id,
+      driverName: null,
+      connection: null,
+      api: null
+    }
+  }
+  return contexts[id];
+}
+
+
+function getApi(socket: any): Bluebird<palantiri.Api> {
+  let api = getContext(socket.id).api;
+  if (api === null) {
+    return Bluebird.reject(new Error("Api is not available"));
+  } else{
+    return Bluebird.resolve(api);
+  }
+}
+
+function bindEvents (socket: any) {
+  socket.on("request", (data: any) => {
+    handleRequest(socket, data);
+  });
+}
+
+function handleRequest (socket: any, request: any) {
+  let requestType = request.type;
+  let requestId = request.id;
+  let data = request.data;
+  let context = getContext(socket.id);
+  console.log(`Received request ${requestId}@${socket.id}`);
+  console.log(requestType);
+  console.log(data);
+
+  let responsePromise: Bluebird<any>;
+  switch(requestType) {
+    case "start-proxy":
+      responsePromise = handleStartProxyRequest(socket, context, data);
+      break;
+    case "get-current-user":
+      responsePromise = handleGetCurrentUserRequest(socket, context, data);
+      break;
+    default:
+      console.log("unknown-type");
+      responsePromise = Bluebird.reject(new Error(`Unknown request type ${requestType}`));
+  }
+
+  responsePromise
+    .then((response: any) => {
+      return JSON.parse(JSON.stringify(response)); // Ensure that there are no cycles
+    })
+    .then(
+      (response: any) => {
+        console.log(`Responding to request ${requestId}@${socket.id}`);
+        console.log(response);
+        socket.emit("request-response", {
+          id: requestId,
+          error: null,
+          data: response
+        })
+      },
+      (error: Error) => {
+        console.log(`An error happened while handling request ${requestId}@${socket.id}`);
+        console.log(error.message);
+        socket.emit("request-response", {
+          id: requestId,
+          error: error.message,
+          data: null
+        });
+      }
+    )
+}
+
+function handleStartProxyRequest (socket: any, context: SocketContext, data: any): Bluebird<any> {
+  return Bluebird.try(() => {
+    if (context.driverName !== null) {
+      throw new Error("Proxy already started");
+    }
+    let driverName = data.driverName;
+    let options = data.options;
+    let connection: palantiri.Connection = null;
+
+    switch(driverName) {
+      case "facebook":
+        connection = new facebookDriver.Connection(options);
+        break;
+      case "skype":
+        connection = new skypeDriver.Connection(options);
+        break;
+      default:
+        throw new Error(`Unknown driver ${driverName}`);
+    }
+
+    context.driverName = driverName;
+    context.connection = connection;
+
+    return Bluebird.resolve(connection.connect())
+      .then((api: palantiri.Api) => {
+        context.api = api;
+        return null;
+      });
+  });
+}
+
+function handleGetCurrentUserRequest (socket: any, context: SocketContext, data: any): Bluebird<any> {
+  return getApi(socket)
+    .then((api: palantiri.Api) => {
+      return api.getCurrentUser();
+    });
+}
